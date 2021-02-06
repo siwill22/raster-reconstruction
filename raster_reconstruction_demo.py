02ac47832d7b5cca89a2ca288e6be304ec766bae
siwill22 <siwill22@gmail.com>
2021-02-06 19:40:52 +1100

    update for python3, gprm

--- raster_reconstruction_demo.py
+++ raster_reconstruction_demo.py
@@ -1,12 +1,9 @@
 import pygplates
 import numpy as np  
 
-import sys
-sys.path.append('../pttx')
-
-from reconstruct_atom_rasters import *
-from reconstruction_classes import *
-from raster_reconstruction_classes import *
+from gprm import ReconstructionModel, GPlatesRaster
+from gprm.utils.raster import reconstruct_raster, xyz2grd
+from gprm.utils.fileio import write_netcdf_grid 
 
 
 # Specify input files and parameters
@@ -19,9 +16,9 @@ raster_file = './earth_relief_10m.nc'
 output_file_template = 'reconstructed_earth_relief_{:0.1f}Ma.nc'
 
 grid_sampling=0.5
-min_reconstruction_time = 10
-max_reconstruction_time = 200
-reconstruction_time_step = 10
+min_reconstruction_time = 10.
+max_reconstruction_time = 200.
+reconstruction_time_step = 10.
 
 # End of input specification
 
@@ -30,7 +27,7 @@ reconstruction_model = ReconstructionModel('M2016')
 reconstruction_model.add_rotation_model(rotation_filename)
 reconstruction_model.add_static_polygons(static_polygon_filename)
 
-ptopo = GplatesRaster(raster_file)
+ptopo = GPlatesRaster(raster_file)
 
 reconstruction_times = np.arange(min_reconstruction_time,
                                  max_reconstruction_time+reconstruction_time_step,
@@ -43,7 +40,7 @@ grid_latitudes = np.arange(-90.,90.0001,grid_sampling)
 
 for reconstruction_time in reconstruction_times:
     
-    print '\nWorking on time {:f} Ma'.format(reconstruction_time)
+    print('\nWorking on time {:f} Ma'.format(reconstruction_time))
     
     from_time = 0.
     to_time = reconstruction_time
@@ -60,6 +57,8 @@ for reconstruction_time in reconstruction_times:
     res = xyz2grd(reconstructed_point_lons,reconstructed_point_lats,reconstructed_point_zvals,
                   grid_longitudes,grid_latitudes)
 
-    ds = xr.DataArray(res, coords=[('y',grid_latitudes), ('x',grid_longitudes)], name='z')
-    ds.to_netcdf(output_file_template.format(reconstruction_time), format='NETCDF4')
-                 
+    #ds = xr.DataArray(res, coords=[('y',grid_latitudes), ('x',grid_longitudes)], name='z')
+    #ds.to_netcdf(output_file_template.format(reconstruction_time), format='NETCDF4')
+    write_netcdf_grid(output_file_template.format(reconstruction_time), 
+                      grid_longitudes, grid_latitudes, res, 
+                      xname='x', yname='y', zname='z', format='NETCDF4')             
